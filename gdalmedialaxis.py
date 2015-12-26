5f48c4b1347ee38984cace7e6ec6032d55bce6bd
cayetanobv <cayetanobv@gmail.com>
2015-12-26 12:09:15 +0100

    first commit

---
+++ gdalmedialaxis.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+#  Author: Cayetano Benavent, 2015.
+#  cayetano.benavent@geographica.gs
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+from osgeo import gdal
+from skimage.morphology import medial_axis
+
+
+def readRaster(input_raster, nodata_val=-9999):
+    src_raster = gdal.Open(input_raster)
+    raster_bnd = src_raster.GetRasterBand(1)
+    raster_values = raster_bnd.ReadAsArray()
+
+    data = raster_values != nodata_val
+
+    src_raster = None
+    
+    print("Raster readed...")
+
+    return data
+
+def computeSkeleton(data):
+    skel, dist = medial_axis(data, return_distance=True)
+
+    dist_on_skel = dist * skel
+    
+    print("Medial axis computed...")
+
+    return(skel, dist, dist_on_skel)
+
+def writeRaster(out_raster, src_raster, skel_data, nodata_val=0):
+    drv_gtif = gdal.GetDriverByName("GTiff")
+    src_ds = gdal.Open(src_raster)
+    dst_raster = drv_gtif.CreateCopy(out_raster, src_ds, 0, options=["COMPRESS=LZW"])
+
+    out_band = dst_raster.GetRasterBand(1)
+    out_band.SetNoDataValue(nodata_val)
+    out_band.WriteArray(skel_data)
+
+    dst_raster = None
+    
+    print("New raster created...")
+
+def getSkeleton(input_raster, out_raster):
+    data = readRaster(input_raster)
+    skel_data, dist, dist_on_skel = computeSkeleton(data)
+    writeRaster(out_raster, input_raster, skel_data)
+
+if __name__ == "__main__":
+    input_raster = "/tmp/input_raster.tif"
+    out_raster = "/tmp/medial_axis.tif"
+
+    getSkeleton(input_raster, out_raster)
