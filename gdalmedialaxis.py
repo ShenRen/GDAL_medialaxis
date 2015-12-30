a8aaa41338f0531f0258c57351cedb004fa763d9
cayetanobv <cayetanobv@gmail.com>
2015-12-30 02:21:07 +0100

    raster to XYZ translation

--- gdalmedialaxis.py
+++ gdalmedialaxis.py
@@ -19,8 +19,39 @@
 #  MA 02110-1301, USA.
 #
 
+import numpy as np
+import pandas as pd
 from osgeo import gdal
 from skimage.morphology import medial_axis
+
+
+def raster2xyz(input_raster, out_xyz, n_band=1, flt_val=1):
+    src_raster = gdal.Open(input_raster)
+    raster_bnd = src_raster.GetRasterBand(n_band)
+    raster_values = raster_bnd.ReadAsArray()
+
+    gtr = src_raster.GetGeoTransform()
+
+    y, x = np.where(raster_values == 1)
+
+    gtr_x = gtr[0] + (x + 0.5) * gtr[1] + (y + 0.5) * gtr[2]
+    gtr_y = gtr[3] + (x + 0.5) * gtr[4] + (y + 0.5) * gtr[5]
+
+    data_vals = np.extract(raster_values == 1, raster_values)
+
+    data_dict = {
+        "x": gtr_x,
+        "y": gtr_y,
+        "z": data_vals
+    }
+
+    df = pd.DataFrame(data_dict)
+
+    df.to_csv(out_xyz, index=False)
+
+    src_raster = None
+
+    print("New XYZ (csv file) created...")
 
 
 def readRaster(input_raster, n_band=1, nodata_val=-9999):
@@ -31,7 +62,7 @@ def readRaster(input_raster, n_band=1, nodata_val=-9999):
     data = raster_values != nodata_val
 
     src_raster = None
-    
+
     print("Raster readed...")
 
     return data
@@ -47,14 +78,14 @@ def writeRaster(out_raster, src_raster, skel_data, n_band=1, nodata_val=0):
 
     src_ds = None
     dst_raster = None
-    
+
     print("New raster created...")
 
 def computeSkeleton(data):
     skel, dist = medial_axis(data, return_distance=True)
 
     dist_on_skel = dist * skel
-    
+
     print("Medial axis computed...")
 
     return(skel, dist, dist_on_skel)
@@ -67,5 +98,7 @@ def getSkeleton(input_raster, out_raster):
 if __name__ == "__main__":
     input_raster = "./data/input_raster.tif"
     out_raster = "/tmp/medial_axis.tif"
+    out_csv = "/tmp/medial_axis.csv"
 
     getSkeleton(input_raster, out_raster)
+    raster2xyz(out_raster, out_csv)
